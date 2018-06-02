d9b08a750f83afaa51d62de4c4cb6f1f424c4b4f
PolGuixe <pol.guixe@gmail.com>
2018-06-02 17:49:47 +0200

    improved code style

--- src/components/ControlSection/styles.js
+++ src/components/ControlSection/styles.js
@@ -3,6 +3,6 @@
 export default (theme: Object):Object => ({
     wrapper: {
         width: '100%',
-        marginBottom: theme.spacing.unit * 2
+        marginBottom: theme.spacing.unit * 2,
     }
 })
